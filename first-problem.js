fa52ab90b767a3e29c0e9db1366729614ab8b390
Sajib8101 <121630475+Sajib8101@users.noreply.github.com>
2023-02-07 22:54:59 +0530

    Assignment 4

---
+++ first-problem.js
@@ -0,0 +1,6 @@
+// First Problem
+let number = 44 ;
+function mindGame(number){
+     console.log(number*3+10/2-5) ;
+}
+mindGame(number);
---
+++ second-problem.js
@@ -0,0 +1,8 @@
+// Second Problem
+let evenOdd = "jack ma" ;
+if (evenOdd.length % 2 == 0){
+   console.log("even") ;
+}
+else{
+    console.log("odd") ;
+}
